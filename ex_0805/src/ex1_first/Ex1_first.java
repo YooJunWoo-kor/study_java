0210068e783be82a0ed034cbafadd6f52264cbfe
pomending <pomending@gmail.com>
2026-08-06 14:32:00 +0900

    0805 수업내용

--- ex_0805/src/ex1_first/Ex1_first.java
+++ ex_0805/src/ex1_first/Ex1_first.java
@@ -5,6 +5,8 @@ public class Ex1_first {
     public static void main(String[] args) { 
         //주석: 컴파일시에 jvm이 인식하지 못하는 
         //개발자들간의 소통을 위한 코드
+
+        //aa
         System.out.println(1 + 1); //System이 클래스(대문자)
         System.out.println("1 + 1 = " + (1 + 1)); 
         System.out.println(1 + 1 + "안녕" + 1 + 1);
